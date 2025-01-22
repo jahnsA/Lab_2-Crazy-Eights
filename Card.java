a09696dd224cab6fa4b98700a67869a5b276ec04
Abby <2abbylouwho@gmail.com>
2025-01-22 09:00:10 -0800

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,30 @@
+// Define enum for Face
+enum Face {
+    ACE, DEUCE, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
+
+// Define enum for Suit
+enum Suit {
+    HEARTS, DIAMONDS, CLUBS, SPADES
+}
+
+public class Card {
+    private final Face face; // Enum for face of card
+    private final Suit suit; // Enum for suit of card
+
+    // Two-argument constructor initializes card's face and suit
+    public Card(Face cardFace, Suit cardSuit) {
+        this.face = cardFace; // Initialize face of card
+        this.suit = cardSuit; // Initialize suit of card
+    }
+
+    // Return String representation of Card
+    public String toString() {
+        return face + " of " + suit;
+    }
+
+    // Get method for face
+    public Face getFace() {
+        return this.face;
+    }
+}
