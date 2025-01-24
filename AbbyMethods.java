82300db7321b8b9a58d120222c92953a91fef9a8
Abby Jahns <2abbylouwho@gmail.com>
2025-01-24 09:20:25 -0800

    added game loop to main

--- AbbyMethods.java
+++ AbbyMethods.java
@@ -10,13 +10,26 @@ public class AbbyMethods {
     testDeck.shuffle();
     System.out.println(testDeck[51].toString());
 
+    //game loop
+    while(playerHand.length == 0 || computerHand.length == 0) {
+        //computer plays or you play
+        //show hand, give options
+        //or pull from deck
+    }
+    if (playerHand.length == 0) {
+        System.out.println("You Win!");
+    }else {
+        System.out.println("You Lose!");
+    }
+
+
     checkForEight();
         
     }//end main method
 
     //check if first card pulled for discard pile is 8
+    //this is assuming the cards are going into a stack, FILO
     //shuffle if true
-    //?Keep calling this until 8 is not the last card?
     public static void checkForEight () {
         while (deck[51].getface() == 8) {
             deck.shuffle();
--- Main.java
+++ Main.java
@@ -14,6 +14,18 @@ public class Main {
         //TEST CODE
         /*System.out.println("Computer hand: " + computerHand);
         System.out.println("Player hand: " + playerHand);*/
+
+        //game loop
+        while(playerHand.size() == 0 || computerHand.size() == 0) {
+            //computer plays or you play
+            //show hand, give options
+            //or pull from deck
+        }
+        if (playerHand.size() == 0) {
+            System.out.println("You Win!");
+        }else {
+            System.out.println("You Lose!");
+        }
         
     }//main method
 }//end class Main
