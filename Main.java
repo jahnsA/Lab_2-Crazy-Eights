68e8a91fc038c33e803422a06c86936709fa8dc9
lizeyliz <alizaeaton@gmail.com>
2025-01-24 09:03:10 -0800

    created player and computer hands

--- Main.java
+++ Main.java
@@ -2,9 +2,18 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         DeckOfCards deck = new DeckOfCards();
+        deck.shuffle();
         ArrayList<Card> computerHand = new ArrayList<Card>();
-        computerHand.add(deck.dealCard());
         ArrayList<Card> playerHand = new ArrayList<Card>();
+        for (int i = 0; i < 5; i++) {
+            computerHand.add(deck.dealCard());
+        }
+        for (int i = 0; i < 5; i++) {
+            playerHand.add(deck.dealCard());
+        }
+        //TEST CODE
+        /*System.out.println("Computer hand: " + computerHand);
+        System.out.println("Player hand: " + playerHand);*/
         
     }//main method
 }//end class Main
